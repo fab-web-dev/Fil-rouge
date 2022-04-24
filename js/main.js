3eb76df985abfbdc2d6254f2c71222c149d08aef
fab-web-dev <97179540+fab-web-dev@users.noreply.github.com>
2022-04-24 19:25:38 +0200

    ajout page contact bootstrap

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const mainButton = document.getElementById("mainButton");
+    if (document.getElementById("mainButton")){
+        const mainButton = document.getElementById("mainButton");
     mainButton.addEventListener("click", function() {
         const mainVideo = document.getElementById("mainVideo");
         mainVideo.classList.add("main-hide");
         document.body.classList.remove("no-scroll");
         scrollTo(0,0);
     });
+    }
+    
+    if (window.location.href.indexOf("no-intro") !== -1){
+        const mainVideo = document.getElementById("mainVideo");
+        mainVideo.classList.add("no-transition");
+        mainVideo.classList.add("main-hide");
+        document.body.classList.remove("no-scroll");
+        scrollTo(0,0);
+    }
+
+    if (window.location.href.indexOf("contact") !== -1)
+    {
+        document.body.classList.add('contact-bg');
+    }
+    else {
+        document.body.classList.remove('contact-bg');
+    }
 });
